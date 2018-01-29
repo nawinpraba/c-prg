84d3ba16b7c9a40ac3b642efb7041b9dc7af67e8
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-29 13:50:14 +0530

    Create n.c

---
+++ n.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("modular",a%4);
+if(a%4==0)
+{
+leep year
+}
+else
+{
+non leep year
+}
+getch();
+}
