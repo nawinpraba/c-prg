83852efa89259d5b9f67bae820353757f1e39e84
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-29 14:05:55 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+int a,b ,c;
+printf("enter the values of a and b");
+scanf("%d,%d",&a,&b);
+printf("addition%d",a+b,c);
+scanf("%d",&c);
+if(c<0)
+{
+negative
+}
+else
+{
+posiitive
+}
+getch();
+}
+
+
