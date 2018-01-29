653a1c0be5747ee22eb82d0f7f39c14cafeefe39
nawinpraba <35774842+nawinpraba@users.noreply.github.com>
2018-01-29 13:33:52 +0530

    Create .c
    its a odd or even

---
+++ .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the value of a");
+scanf("%d",&a);
+printf("modular",a%2);
+if(a%2==0)
+{
+even 
+}
+else
+{
+odd
+}
+getch
+}
